c256365d79d13b402daec6b40b79bcac2fd4f9d2
Adam <adamlederer@live.ca>
2022-11-08 20:34:45 -0700

    Made notes for functions

---
+++ js/apps.js
@@ -0,0 +1,37 @@
+// Function for category number 1
+// function greetUser(){
+//      console.log(`Hello User`);
+    // console.log(`Geetings, please choose one of the folling.`);
+    // console.log(`1. Add 2 numbers`);
+    // console.log(`2. Divide 2 numbers`);
+// }
+// greetUser();
+
+
+
+// Function for category number 2
+// function greetSpecificUser(username) {
+//     console.log(`Hello, ${username}. How was your day?`);
+// }
+
+// greetSpecificUser(`Timothy`);
+// greetSpecificUser(`Aaron`);
+
+
+// Function for category number 3
+// function getWinningNumber() {
+//     let winner = 42;
+//     return winner;
+// }
+// let winningNumber = getWinningNumber();
+// console.log(winningNumber);
+
+
+// Function for category number 4
+// function getSum(num1, num2) {
+//     let result = num1 + num2;
+//     return result;
+// }
+// console.log(getSum(1,2));
+// console.log(getSum(105,203));
+// console.log(getSum(1,-5));
